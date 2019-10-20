8fc57fcfdf7b24a6cc876344a27e3ea6580f82f5
InSeoung95 <55937177+InSeoung95@users.noreply.github.com>
2019-10-20 22:59:55 +0900

    플레이어 캐릭터
    근접 플레이어 캐릭터 melee_01
    원거리 플레이어 캐릭 magic_01

---
+++ Assets/HeroEditor4D/Common/CharacterScripts/CharacterState.cs
@@ -0,0 +1,12 @@
+﻿namespace Assets.HeroEditor4D.Common.CharacterScripts
+{
+	public enum CharacterState
+	{
+		Idle 	= 0,
+		Ready 	= 1,
+		Walk 	= 2,
+		Run 	= 3,
+		Jump 	= 4,
+		Climb 	= 5
+	}
+}
