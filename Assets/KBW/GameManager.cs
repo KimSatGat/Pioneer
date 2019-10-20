7d13f573a8b9b991f82e178d921a49280555de6a
Kim Byeong Wan <byeongkkog@naver.com>
2019-10-20 23:02:57 +0900

    가상 조이스틱 구현

---
+++ Assets/KBW/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        SettingResolution();
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // 해상도 설정
+    void SettingResolution()
+    {
+        Screen.SetResolution(Screen.width, Screen.width * 16 / 9, true);
+    }
+}
---
+++ Assets/KBW/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Joystick joystick; 
+    public float moveSpeed;     // 플레이어 이동속도
+
+    private Vector3 moveVector; // 플레이어 이동벡터    
+    
+    void Start()
+    {
+        moveVector = Vector3.zero;  // 플레이어 이동벡터 초기화
+    }
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    
+    // 조이스틱 입력값 받아오기
+    public void HandleInput()
+    {
+        float h = joystick.GetHorizontalValue();
+        float v = joystick.GetVerticalValue();
+        Vector3 moveDir = new Vector3(h, v, 0).normalized;
+
+        moveVector = moveDir;
+    }
+
+    // 플레이어 이동    
+    public void Move()
+    {
+        transform.Translate(moveVector * moveSpeed * Time.deltaTime);
+    }
+}
