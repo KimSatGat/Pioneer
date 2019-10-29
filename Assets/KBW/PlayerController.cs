ec735fdc1494d769df4799b197ee832d5b482d16
Kim Byeong Wan <byeongkkog@naver.com>
2019-10-29 18:09:53 +0900

    조이스틱에 따른 플레이어 방향 설정
    방향벡터의 x값이 0 이상이면 오른쪽, 0 미만이면 왼쪽 바라보기

--- Assets/KBW/PlayerController.cs
+++ Assets/KBW/PlayerController.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    enum PlayerState { idle, run};
+
     public Joystick joystick; 
     public float moveSpeed;     // 플레이어 이동속도
 
-    private Vector3 moveVector; // 플레이어 이동벡터    
+    private Vector3 moveVector; // 플레이어 이동벡터
+    private PlayerState playerState; // 플레이어 상태
+    private Animator animator;  // 플레이어 애니메이터
+
     
     void Start()
     {
+        playerState = PlayerState.idle;
         moveVector = Vector3.zero;  // 플레이어 이동벡터 초기화
+
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -21,7 +29,8 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Move();
+        PlayerAnimation();
+        PlayerMove();
     }
     
     // 조이스틱 입력값 받아오기
@@ -30,11 +39,25 @@ public class PlayerController : MonoBehaviour
         Vector2 moveDir = joystick.GetPlayerDir();
 
         moveVector = moveDir;
+        Debug.Log(moveVector.x);
     }
 
     // 플레이어 이동    
-    public void Move()
+    private void PlayerMove()
     {
         transform.Translate(moveVector * moveSpeed * Time.deltaTime);
+    }
+
+    // 플레이어 애니메이션
+    private void PlayerAnimation()
+    {
+        if(moveVector.x >= 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
     }
 }
